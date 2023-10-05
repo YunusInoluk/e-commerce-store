f69a0cca453d711318268c5d752a344105f36dfa
Yunus İnoluk <yunusinoluk@gmail.com>
2023-10-05 12:41:21 +0300

    feat: product detail page added

---
+++ components/product-info.tsx
@@ -0,0 +1,43 @@
+import { Product } from "@/types";
+import { ShoppingCart } from "lucide-react";
+import Button from "./ui/button";
+import Currency from "./ui/currency";
+
+interface ProductInfoProps {
+  data: Product;
+}
+
+const ProductInfo: React.FC<ProductInfoProps> = ({ data }) => {
+  return (
+    <div>
+      <h1 className="font-bold text-3xl text-gray-900">{data.name}</h1>
+      <div className="flex items-end justify-between mt-3">
+        <p className="text-2xl text-gray-900">
+          <Currency value={data.price} />
+        </p>
+      </div>
+      <hr className="my-4" />
+      <div className="flex flex-col gap-y-5">
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Size:</h3>
+          <div>{data?.size?.name}</div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <h3 className="font-semibold text-black">Color:</h3>
+          <div
+            className="w-6 h-6 rounded-full border border-gray-600"
+            style={{ backgroundColor: data?.color?.value }}
+          />
+        </div>
+      </div>
+      <div className="flex items-center gap-x-3 mt-10">
+        <Button className="flex item-center gap-x-2">
+          Add to Cart
+          <ShoppingCart />
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductInfo;
