574164952ecf182643793d6137e49044e20c789b
Yunus İnoluk <yunusinoluk@gmail.com>
2023-11-27 16:56:26 +0300

    fix: onCheckout typo fix

--- app/(routes)/cart/components/summary.tsx
+++ app/(routes)/cart/components/summary.tsx
@@ -28,7 +28,7 @@ const Summary = () => {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
       {
-        producIds: items.map((item) => item.id),
+        productIds: items.map((item) => item.id),
       }
     );
     window.location = response.data.url;
